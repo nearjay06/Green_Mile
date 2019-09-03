5c07ea09646175a198d2fa390c497d83e5db0a06
nearjay06 <nearjoan2018@gmail.com>
2019-09-04 00:15:08 +0300

    modify models,style and dash files

--- deliver/project/models.py
+++ deliver/project/models.py
@@ -4,14 +4,16 @@ from datetime import datetime
 from project import db
 from project import login
 
-class User(UserMixin,db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True,unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    image_file= db.Column(db.String(20), nullable = False, default='default.jpg')
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return 'User {}'.format(self.username)
+        return '<User {}>'.format(self.username)
 
     def set_password(self, password_hash):
         self.password_hash = generate_password_hash(password)
@@ -27,7 +29,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}'.fromat(self.body)
+        return '<Post {}'.format(self.body)
 
 
 
