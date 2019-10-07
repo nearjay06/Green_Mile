8e45d1eea393ecc98712387e565bcda933927942
nearjay06 <nearjoan2018@gmail.com>
2019-10-07 22:37:56 +0300

    modify css files[Finishes #168228519]

--- deliver/project/routes.py
+++ deliver/project/routes.py
@@ -37,31 +37,22 @@ def signup():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # form = LoginForm()
-    # error=None
-    # if request.method == 'POST':
-    #     if request.form['username'] != 'user' or \
-    #             request.form['password'] != 'password':
-    #         error = 'Invalid username or password'
-    #     else:
-    #         flash('Successfully signed in')
-    #         return redirect(url_for('dash'))
-    # return render_template('login.html', error=error,form=form)
-
-    if current_user.is_authenticated:
-        return redirect(url_for('dash'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+       return redirect(url_for('dash'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.username.data).first()
+    #     if user is None or not user.check_password(form.password.data):
+    #         flash('Invalid username or password')
+    #         return redirect(url_for('login'))
+    #     else:
+    #         flash('Successfully signed in')
+    #     return redirect(url_for('dash'))
+    # return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/dash', methods=['GET', 'POST'])
 def dash():
